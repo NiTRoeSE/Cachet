8e3280dc144be4b53570872aa460a4050dffd37f
NiTRoeSE <michael.schunk@medialyse-photography.de>
2021-01-13 21:01:17 +0100

    Update ComponentStatusChangedNotification.php

--- app/Notifications/Component/ComponentStatusChangedNotification.php
+++ app/Notifications/Component/ComponentStatusChangedNotification.php
@@ -89,9 +89,11 @@ class ComponentStatusChangedNotification extends Notification
                 'componentName'          => $this->component->name,
                 'content'                => $content,
                 'unsubscribeText'        => trans('cachet.subscriber.unsubscribe'),
-                'unsubscribeUrl'         => cachet_route('subscribe.unsubscribe', $notifiable->verify_code),
+                /**'unsubscribeUrl'         => cachet_route('subscribe.unsubscribe', $notifiable->verify_code),**/
+                'unsubscribeUrl'         => 'https://status.jsp.jena.de'.'subscribe.unsubscribe'.$notifiable->verify_code,
                 'manageSubscriptionText' => trans('cachet.subscriber.manage_subscription'),
-                'manageSubscriptionUrl'  => cachet_route('subscribe.manage', $notifiable->verify_code),
+                /**'manageSubscriptionUrl'  => cachet_route('subscribe.manage', $notifiable->verify_code),**/
+                'manageSubscriptionUrl' => 'https://status.jsp.jena.de/'.'subscribe.manage'.$notifiable->verify_code,
             ]);
     }
 
